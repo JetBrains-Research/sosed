9e79ea9578fd10579dbe3a5c7c588bfd5886aa89
egor-bogomolov <egor.bogomolov@jetbrains.com>
2020-05-24 02:59:00 +0300

    DJ Khaled

--- similar_repositories/setup_tokenizer.py
+++ similar_repositories/setup_tokenizer.py
@@ -8,7 +8,10 @@ def setup_tokenizer() -> None:
     Clone GitHub repository with tokenizer and setup it.
     :return: None.
     """
-    os.system(f'git clone --recurse-submodules --shallow-submodules --branch={TOKENIZER_VERSION} {TOKENIZER_URL} {TOKENIZER_DIR}')
+    os.system(f'git clone --branch={TOKENIZER_VERSION} {TOKENIZER_URL} {TOKENIZER_DIR}')
+    os.chdir(TOKENIZER_DIR)
+    os.system('git submodule update --init --recursive --depth 1')
+    os.chdir('..')
 
 
 if __name__ == '__main__':
