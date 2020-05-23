c6cd3ad770b022280dcfc6423ed8193f6a603224
egor-bogomolov <egor.bogomolov@jetbrains.com>
2020-05-24 02:00:16 +0300

    Use released version of tokenizer

--- similar_repositories/setup_tokenizer.py
+++ similar_repositories/setup_tokenizer.py
@@ -8,9 +8,9 @@ def setup_tokenizer() -> None:
     Clone GitHub repository with tokenizer and setup it.
     :return: None.
     """
-    os.system(f'git clone --recurse-submodules {TOKENIZER_URL} {TOKENIZER_DIR}')
+    os.system(f'git clone --recursive --shallow-submodules {TOKENIZER_URL} {TOKENIZER_DIR}')
     os.chdir(TOKENIZER_DIR)
-    os.system(f'git checkout {TOKENIZER_COMMIT}')
+    os.system(f'git checkout {TOKENIZER_VERSION} --quiet')
     os.chdir('..')
 
 
--- similar_repositories/utils.py
+++ similar_repositories/utils.py
@@ -7,8 +7,8 @@ from typing import List
 DIM = 256
 
 TOKENIZER_DIR = Path('tokenizer')
-TOKENIZER_URL = 'https://github.com/areyde/topic-dynamics.git'
-TOKENIZER_COMMIT = 'a8f8c0860d556eb98da32ef9bfec7ee1966fe98a'
+TOKENIZER_URL = 'https://github.com/JetBrains-Research/identifiers-extractor.git'
+TOKENIZER_VERSION = 'v1.0.0'
 
 DATA_LINK = 'https://drive.google.com/uc?id=1CTO24nZtMyHVQ43mhljfH2qN_QWNIZGo'
 DATA_ARCHIVE = 'data.tar.gz'
